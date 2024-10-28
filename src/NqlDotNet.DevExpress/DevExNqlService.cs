08fba466be44c3f5ea6de90e9df0ed350146f66e
Jose Ojeda <joche.ojeda@bitframeworks.com>
2024-10-28 23:08:32 +0300

    CriteriaResult

--- src/NqlDotNet.DevExpress/DevExNqlService.cs
+++ src/NqlDotNet.DevExpress/DevExNqlService.cs
@@ -3,15 +3,18 @@ using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel;
 using OpenAI;
 using System.Diagnostics;
-
+using System.Text;
+using MongoDB.Bson.IO;
+using MongoDB.Bson;
+using System.Text.Json;
 namespace NqlDotNet.DevExpress
 {
-
+#pragma warning disable SKEXP0010
     public class DevExNqlService : INqlService
     {
         IChatCompletionService? chatService;
         Kernel? sk;
-
+        const string ResultFormat = "{\"RootEntity\": null, \"Criteria\": null}";
         public DevExNqlService()
         {
             var clientOpenAi = new OpenAIClient(new System.ClientModel.ApiKeyCredential(Environment.GetEnvironmentVariable("OpenAiTestKey")));
@@ -20,17 +23,18 @@ namespace NqlDotNet.DevExpress
             sk = KernelBuilder.Build();
             chatService = sk.GetRequiredService<IChatCompletionService>();
         }
-        public async Task<string> NlToCriteria(string Nlq, string Schema, string Doc)
+        public async Task<CriteriaResult> NlToCriteria(string Nlq, string Schema, string Doc)
         {
             //KernelArguments arguments = new(new OpenAIPromptExecutionSettings { MaxTokens = 500, Temperature = 0.5 }) { { "Nlq", Nlq }, { "Doc", Doc }, { "Schema", Schema } };// new() { { "topic", "sea" } };
-            KernelArguments arguments = new(new OpenAIPromptExecutionSettings { MaxTokens = 500, Temperature = 0.5 });
+            KernelArguments arguments = new(new OpenAIPromptExecutionSettings { MaxTokens = 500, Temperature = 0.5, ResponseFormat="json_object" });
             //Given the DevExpress Criteria Language syntax documentation and the.
-            FunctionResult value = await sk.InvokePromptAsync($"Given the DevExpress Criteria Language syntax documentation {Doc} and JSON schema of classes {Schema}, transform a natural language query #{Nlq}# into a criteria string suitable for querying data,return only the criteria without any extra text or explanation", arguments);
+            FunctionResult value = await sk.InvokePromptAsync($"Given the DevExpress Criteria Language syntax documentation {Doc} and JSON schema of classes {Schema}, transform a natural language query #{Nlq}# into a criteria string suitable for querying data,return only the criteria without any extra text or explanation, the result should be a json with this structure {ResultFormat}", arguments);
+            //var stringvalue = value.ToString().Split(Environment.NewLine);
+            //var cleanvalue=value.ToString().Replace("```json", "").Replace("```", ""); ;
             Debug.WriteLine(value);
 
-
-
-            return value.ToString();
+            CriteriaResult result = JsonSerializer.Deserialize<CriteriaResult>(value.ToString());
+            return result;
         }
         public async Task<string> CriteriaToNl(string Criteria, string Schema, string Doc)
         {
--- src/NqlDotNet/INqlService.cs
+++ src/NqlDotNet/INqlService.cs
@@ -6,6 +6,6 @@ namespace NqlDotNet
     public interface INqlService
     {
         Task<string> CriteriaToNl(string Criteria, string Schema, string Doc);
-        Task<string> NlToCriteria(string Nlq, string Schema, string Doc);
+        Task<CriteriaResult> NlToCriteria(string Nlq, string Schema, string Doc);
     }
 }
